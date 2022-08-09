cf4e069b02da6e0f7d7b75efdf4942f94d762c8b
Elizaveta <65027713+ElizavetaKubik@users.noreply.github.com>
2022-08-09 23:52:58 +0300

    feature: launcher weapon

--- ShootThemUp/Source/ShootThemUp/Public/Weapon/STUBaseWeapon.h
+++ ShootThemUp/Source/ShootThemUp/Public/Weapon/STUBaseWeapon.h
@@ -25,14 +25,11 @@ protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category="Component")
 	USkeletalMeshComponent* WeaponMesh;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
 	FName MuzzleSocketName = "MuzzleSocket";
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
 	float TraceMaxDistance = 1000.0f;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
-	float DamageAmount = 10.0f;
 	
 	virtual void BeginPlay() override;
 
@@ -43,5 +40,4 @@ protected:
 	bool GetPlayerViewPoint(FVector& ViewLocation, FRotator& ViewRotation) const;
 	FVector GetMuzzleWorldLocation() const;
 	void MakeHit(FHitResult& HitResult, const FVector& TraceStart, const FVector& TraceEnd) const;
-	void MakeDamage(const FHitResult& HitResult);
 };
