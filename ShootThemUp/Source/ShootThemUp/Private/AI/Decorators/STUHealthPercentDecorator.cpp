dfa5247974ae02e9af4247c2c5619ccfb5ed4e6c
Elizaveta <kubikelizavetaofficial@gmail.com>
2022-08-23 19:22:04 +0300

    feature: ai health pickup

--- ShootThemUp/Source/ShootThemUp/Private/AI/Decorators/STUHealthPercentDecorator.cpp
+++ ShootThemUp/Source/ShootThemUp/Private/AI/Decorators/STUHealthPercentDecorator.cpp
@@ -2,4 +2,24 @@
 
 
 #include "AI/Decorators/STUHealthPercentDecorator.h"
+#include "Components/STUHealthComponent.h"
+#include "STUUtils.h"
+#include "AIController.h"
+
+USTUHealthPercentDecorator::USTUHealthPercentDecorator()
+{
+	NodeName = "Health Percent";
+}
+
+bool USTUHealthPercentDecorator::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const
+{
+	const auto Controller = OwnerComp.GetAIOwner();
+	if(!Controller) return false;
+
+	const auto HealthComponent = STUUtils::GetSTUPlayerComponent<USTUHealthComponent>(Controller->GetPawn());
+	if(!HealthComponent || HealthComponent->IsDead()) return false;
+	
+	return HealthComponent->GetHealthPercent() < HealthPercent;
+}
+
 
--- ShootThemUp/Source/ShootThemUp/Public/AI/Decorators/STUHealthPercentDecorator.h
+++ ShootThemUp/Source/ShootThemUp/Public/AI/Decorators/STUHealthPercentDecorator.h
@@ -7,8 +7,17 @@
 #include "STUHealthPercentDecorator.generated.h"
 
 UCLASS()
-class SHOOTTHEMUP_API USTOHealthPercentDecorator : public UBTDecorator
+class SHOOTTHEMUP_API USTUHealthPercentDecorator : public UBTDecorator
 {
 	GENERATED_BODY()
+
+public:
+	USTUHealthPercentDecorator();
+
+protected:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="AI")
+	float HealthPercent = 0.6f;
 	
+	virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const override;
 };
